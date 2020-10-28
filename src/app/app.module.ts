c358ad5367663e9b221c7592f37da633aa01d80f
Miguel Angel <miguel.teletor@atmira.com>
2020-10-28 11:22:28 +0100

    feat: connected api susccessfully with HttpclientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+//components
 import { AppComponent } from './app.component';
 import { ArtistComponent } from './components/artist/artist.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+//routs
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,12 +17,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     ArtistComponent,
     HomeComponent,
     SearchComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    console.log('api connected successfully');
+    this.http
+      .get('https://restcountries.eu/rest/v2/lang/en')
+      .subscribe((countryEnglishSpeakers) => {
+        console.log(countryEnglishSpeakers);
+      });
   }
 
+  ngOnInit(): void {}
 }
