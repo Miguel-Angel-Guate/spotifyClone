14f2dfc50744bce2c7e63c081431f19b5d12cfbf
Miguel Angel <miguel.teletor@atmira.com>
2020-10-28 16:22:53 +0100

    feat: search of spotify is work, maybe need a little improve

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SpofityService } from 'src/app/services/spofity.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SearchComponent {
+  constructor(private spotify: SpofityService) {}
+  artistss: any[] = [];
+  searching(findanything: string) {
+    console.log(findanything);
+    this.spotify.getArtist(findanything).subscribe((answer: any) => {
+      console.log('the answer', answer.artists.items);
+      this.artistss = answer.artists.items;
+    });
   }
-
 }
--- src/app/services/spofity.service.ts
+++ src/app/services/spofity.service.ts
@@ -9,12 +9,26 @@ export class SpofityService {
   getNewReleases() {
     const headers = new HttpHeaders({
       Authorization:
-        'Bearer BQAAzI871IbAd4sF_hBbMHlAaNvk-LzRC6kKsygD_alBu6fcyiYqQuP33lwm3ouabxT9Re5PPq8oHdDUM7k',
+        'Bearer BQA9hWlvr8wruCxDlWh99NRBUUPvsV2S1BapXdL7Mqko9cXWhP-Rg1Cy0Dt6yxdqQWKPCK3TOH_gWr7VKYg',
       'Content-Type': 'text/plain; charset=utf-8',
     });
     responseType: 'text' as 'json';
     return this.http.get(
       'https://api.spotify.com/v1/browse/new-releases?limit=10',
+      {
+        headers,
+      }
+    );
+  }
+  getArtist(findAnything: string) {
+    const headers = new HttpHeaders({
+      Authorization:
+        'Bearer BQA9hWlvr8wruCxDlWh99NRBUUPvsV2S1BapXdL7Mqko9cXWhP-Rg1Cy0Dt6yxdqQWKPCK3TOH_gWr7VKYg',
+      'Content-Type': 'text/plain; charset=utf-8',
+    });
+    responseType: 'text' as 'json';
+    return this.http.get(
+      `https://api.spotify.com/v1/search?q=${findAnything}&type=artist&limit=10`,
       {
         headers,
       }
